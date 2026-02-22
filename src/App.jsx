a9673ef3c959381e8fb8ed00de796c2bd35ade0d
Xeno-0 <vansh.vashist2005@gmail.com>
2026-02-22 18:47:22 +0530

    fix: resolve double scrollbar issue by moving overflow settings to root

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   }, []);
 
   return (
-    <div className="relative w-full min-h-screen overflow-x-hidden">
+    <div className="relative w-full min-h-screen">
       <Navbar />
       <Hero />
       <HowItWorks />
